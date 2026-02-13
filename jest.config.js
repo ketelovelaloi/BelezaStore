58508e655c7ce4cd473da4beda335d6bfa003db7
ketelovelaloi <laloiketelove@gmail.com>
2026-02-12 16:24:27 -0800

    Add jest.config.js for React Native testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+};
